50009e22c4af1b816521e4490914c5c82352aab3
Sean Yu <seansu4you87@gmail.com>
2013-08-27 10:30:38 -0700

    Boosting problem 1

--- 001.rb
+++ 001.rb
@@ -1,15 +1,12 @@
 =begin
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+If we list all the natural numbers below 10 that are multiples 
+of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 =end
 
-sum = 0
-
-(1...1000).each do |n|
-  if (n % 3 == 0 || n % 5 == 0)
-    sum += n
-  end
+def multiple_of_3_or_5?(num)
+  num % 3 == 0 || num % 5 == 0
 end
 
-puts sum
+puts (1...1000).select{ |num| multiple_of_3_or_5?(num) }.reduce(:+)
