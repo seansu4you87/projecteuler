5eeb05bc70c490a769050142b4e264fda3aac2d5
Sean Yu <seansu4you87@gmail.com>
2013-09-16 14:36:23 -0700

    Massive speed improvement

--- 012.go
+++ 012.go
@@ -1,33 +1,45 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "math"
+)
 
 func triangle(n int) (triangle, numFactors int) {
 	for i := 0; i <= n; i++ {
 		triangle += i
 	}
 
-	for i := 1; i <= triangle; i++ {
+	for i := 1; i <= int(math.Sqrt(float64(triangle))); i++ {
 		if triangle%i == 0 {
-			numFactors++
+			numFactors += 2
 		}
 	}
 
 	return triangle, numFactors
 }
 
-func printTriangle(n int) {
+func printTriangle(n int, c chan []int) {
 	triangle, numFactors := triangle(n)
 
-	if numFactors >= 200 {
-		fmt.Printf("∆ %v: %v has %v factors\n", n, triangle, numFactors)
-	}
+	// if numFactors >= 200 {
+		// fmt.Printf("∆ %v: %v has %v factors\n", n, triangle, numFactors)
+    // c <- fmt.Sprintf("∆ %v: %v has %v factors\n", n, triangle, numFactors)
+    c <- []int{n, triangle, numFactors}
+	// }
 }
 
 func main() {
+  c := make(chan []int)
+
 	do := true
-  // 76576500
-	for i := 7776; do; i++ {
-		printTriangle(i)
+  // 12375th triangle number: 76576500, has over 500 factors
+	for i := 0; do; i++ {
+		go printTriangle(i, c)
+    result := <- c
+    if result[2] >= 500 {
+      do = false
+      fmt.Printf("∆ %v: %v has %v factors\n", result[0], result[1], result[2])
+    }
 	}
 }
